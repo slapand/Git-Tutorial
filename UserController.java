e4db8d13e62d482e2baba9cce6e27cbe6e92b820
panda0104 <anyongxin0104@gmail.com>
2021-03-16 16:02:40 +0900

    'insert'

--- UserController.java
+++ UserController.java
@@ -6,4 +6,10 @@ public class UserController {
 		logger.info("경로 : login");
 		return "user/login";
 	}
+
+	@RequestMapping("/user/insert.do")
+	public String insert(){
+		logger.info("경로 : insert");
+		return "user/insert";
+	}
 }
